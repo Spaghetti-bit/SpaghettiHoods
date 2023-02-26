16c67fd35a4b8fb09e5994b21d8110e9baa9585e
Spaghetti-bit <Cvaughn@student.neumont.edu>
2023-02-25 19:56:22 -0700

    Baril top shock rebalance

--- Suits/S13/Baril/config.cpp
+++ Suits/S13/Baril/config.cpp
@@ -12,7 +12,73 @@ class CfgPatches
 class CfgVehicles
 {
 	class S13HeadDummy_Base;
-	class b5m_patch;
+	class Clothing;
+	class b5m_patch: Clothing
+	{
+		class GlobalArmor
+		{
+			class Projectile
+			{
+				class Health
+				{
+					damage = 0.25;
+				};
+				class Blood
+				{
+					damage = 0;
+				};
+				class Shock
+				{
+					damage = 0.25;
+				};
+			};
+			class Melee
+			{
+				class Health
+				{
+					damage = 0.25;
+				};
+				class Blood
+				{
+					damage = 0;
+				};
+				class Shock
+				{
+					damage = 0.25;
+				};
+			};
+			class Infected
+			{
+				class Health
+				{
+					damage = 0.25;
+				};
+				class Blood
+				{
+					damage = 0;
+				};
+				class Shock
+				{
+					damage = 0.25;
+				};
+			};
+			class FragGrenade
+			{
+				class Health
+				{
+					damage = 0.5;
+				};
+				class Blood
+				{
+					damage = 0;
+				};
+				class Shock
+				{
+					damage = 0.26;
+				};
+			};
+		};
+	};
 
     class s13_baril_freedom: b5m_patch
 	{
